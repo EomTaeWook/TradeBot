b8e43764a62523670239268e03a7e7e73ddd952e
EomTaeWook <trim0917@gmail.com>
2025-01-14 00:47:19 +0900

    웹 소켓 클라이언트 구조 잡기

---
+++ WebSocket/Session.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.WebSockets;
+
+namespace Dignus.WebSockets
+{
+    public class Session
+    {
+        private ClientWebSocket _webSocket;
+        private int _disposed = 0;
+        public Session()
+        {
+        }
+        public void Send(ArraySegment<byte> bytes, WebSocketMessageType webSocketMessageType)
+        {
+            _webSocket.SendAsync(bytes, webSocketMessageType, true, CancellationToken.None);
+        }
+
+        public void SetClientWebSocket(ClientWebSocket webSocket)
+        {
+            _webSocket = webSocket;
+        }
+        public bool IsDispose()
+        {
+            return _disposed == 1;
+        }
+        public void Dispose()
+        {
+            if (_disposed == 1)
+            {
+                return;
+            }
+
+            if (Interlocked.CompareExchange(ref _disposed, 1, 0) == 0)
+            {
+                _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
+                _webSocket.Dispose();
+
+                _webSocket = null;
+            }
+        }
+
+    }
+}
---
+++ WebSocket/WebSocketClient.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.WebSockets;
+
+namespace Dignus.WebSockets
+{
+    public class WebSocketClientBase
+    {
+        private int _connected = 0;
+        private Session _session;
+        public async Task ConnectAsync(string uriString, int timeout = 5000)
+        {
+            if (Interlocked.CompareExchange(ref _connected, 1, 0) != 0)
+            {
+                throw new Exception("already connect client");
+            }
+
+            var clientWebSocket = new ClientWebSocket();
+
+            try
+            {
+                var timeoutTokenSource = new CancellationTokenSource(timeout);
+                await clientWebSocket.ConnectAsync(new Uri(uriString), timeoutTokenSource.Token);
+            }
+            catch (Exception)
+            {
+                _connected = 0;
+                throw;
+            }
+
+            _session.SetClientWebSocket(clientWebSocket);
+        }
+        public void Send()
+        {
+
+        }
+
+        public void Close()
+        {
+            if (_session == null)
+            {
+                return;
+            }
+            if (_session.IsDispose() == false)
+            {
+                _session.Dispose();
+            }
+            _connected = 0;
+        }
+
+    }
+
+}
